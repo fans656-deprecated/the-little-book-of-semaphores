7190356060ebb0154fe74d96c4fb80c411d90eb0
fans656 <fans656@yahoo.com>
2015-04-26 17:02:10 +0800

    3.7.4: mutex as lock and token

--- t.py
+++ t.py
@@ -2,60 +2,39 @@ from threading import Semaphore, Thread
 import threading
 import time
 
-class Barrier(object):
-
-    def __init__(self, nThread=2):
-        self.nThread = nThread
-        self.nArrive = 0
-        self.nArrive_lock = Semaphore(1)
-        self.allArrive = Semaphore(0)
-
-    def wait(self):
-        self.nArrive_lock.acquire()
-        self.nArrive += 1
-        self.nArrive_lock.release()
-        if self.nArrive == self.nThread:
-            self.nArrive = 0
-            for _ in range(self.nThread - 1):
-                self.allArrive.release()
-        else:
-            self.allArrive.acquire()
-
-class Group(object):
-
-    def __init__(self, nMember=2):
-        self.nMember = nMember
-        self.arrives = [Semaphore(0) for _ in range(nMember)]
-        self.finishes = [Semaphore(0) for _ in range(nMember)]
-        self.enters = [Semaphore(1) for _ in range(nMember)]
-
-    def wait(self, iMember):
-        self.arrives[iMember].release()
-        for i in range(self.nMember):
-            if i == iMember:
-                continue
-            self.arrives[i].acquire()
-        self.enters[iMember].acquire()
-
-    def leave(self, iMember):
-        self.finishes[iMember].release()
-        for i in range(self.nMember):
-            if i == iMember:
-                continue
-            self.finishes[i].acquire()
-        self.enters[iMember].release()
-
-g = Group()
+nBoy = 0
+nGirl = 0
+token = Semaphore(1)
+bQueue = Semaphore(0)
+gQueue = Semaphore(0)
+gFinish = Semaphore(0)
 
 def a():
-    g.wait(0)
+    global nGirl, nBoy
+    token.acquire()
+    if nGirl > 0:
+        nGirl -= 1
+        gQueue.release()
+    else:
+        nBoy += 1
+        token.release()
+        bQueue.acquire()
     print 'B'
-    g.leave(0)
+    gFinish.acquire()
+    token.release()
 
 def b():
-    g.wait(1)
+    global nGirl, nBoy
+    token.acquire()
+    if nBoy > 0:
+        nBoy -= 1
+        bQueue.release()
+    else:
+        nGirl += 1
+        token.release()
+        gQueue.acquire()
     print 'G'
-    g.leave(1)
+    gFinish.release()
 
 boys = [Thread(target=f) for f in [a] * 5]
 girls = [Thread(target=f) for f in [b] * 5]
@@ -63,12 +42,12 @@ print '== 5 boys are waiting'
 for boy in boys:
     boy.start()
 
-time.sleep(1)
+time.sleep(0.01)
 print '== 2 girls arriving'
 for girl in girls[:2]:
     girl.start()
 
-time.sleep(1)
+time.sleep(0.01)
 print '== 3 more girls arriving'
 for girl in girls[2:]:
     girl.start()
